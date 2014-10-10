d8eb9b91dcb425d5ae33513671e36d6195d15e11
David Conner <dconner.pro@gmail.com>
2014-10-10 04:25:44 -0400

    * partially complete firebase auth code.

--- app/js/services.js
+++ app/js/services.js
@@ -9,6 +9,145 @@ function EnvService (CFG) {
 
 angular.module('app').service('EnvService', EnvService);
 
+// wait wtf, FB simple login is deprecated as of 10/3/14? wtf?
+
+//function AuthService($q, $firebase,
+
+//function AuthService ($q, $firebase, $firebaseSimpleLogin, EnvService, Restangular) {
+//
+//  //TODO: setup an onAuth callback?
+//  //TODO: setup oauth
+//  var AuthService = {},
+//      furl = EnvService.fbaseUrl,
+//      fref = new Firebase(EnvService.fbaseUrl),
+//      fbaseSimpleLogin = $firebaseSimpleLogin(fref),
+//      usersUrl = furl + '/users',
+//      usersRef = $firebase(new Firebase(usersUrl));
+//
+//  AuthService.getCurrentUser = function () {
+//    var deferred = $q.defer();
+//
+//    fbaseSimpleLogin.$getCurrentUser().then(deferred.resolve, deferred.reject);
+//    deferred.promise.then(function (currentUser) {
+//      if (currentUser) {
+//        Restangular.setDefaultHeaders({"Authorization": currentUser.firebaseAuthToken});
+//      }
+//    });
+//
+//    return deferred.promise;
+//  };
+//
+//  AuthService.get = function () {
+//    var deferred = $q.defer();
+//
+//    AuthService.getCurrentUser().then(function (currentUser) {
+//      deferred.resolve(currentUser ? $firebase(new Firebase(usersUrl + '/' + currentUser.id)) : null);
+//    }, deferred.reject);
+//
+//    return deferred.promise;
+//  };
+//
+//  AuthService.getRef = function () {
+//    var deferred = $q.defer();
+//
+//    AuthService.getCurrentUser().then(function (user) {
+//      deferred.resolve(user ? usersRef.$child(user.id) : null);
+//    }, deferred.reject);
+//
+//    return deferred.promise;
+//  };
+//
+//  AuthService.create = function (user) {
+//    var deferred = $q.defer();
+//
+//    fbaseSimpleLogin.$createUser(user.email, user.password).then(function (user) {
+//      var userRef = $firebase(new Firebase(usersUrl + '/' + user.id));
+//      userRef.email = user.email;
+//      userRef.$save().then(deferred.resolve, deferred.reject);
+//    }, deferred.reject);
+//
+//    return deferred.promise;
+//  };
+//
+//  AuthService.remove = function () {
+//
+//  };
+//
+//  AuthService.reset = function (email) {
+//    var deferred = $q.defer();
+//
+//    fbaseSimpleLogin.$sendPasswordResetEmail(email);
+//
+//    // firebaseSimpleLogin.$resetPassword has not yet been implemented in angularfire. We're going it alone.
+//    var auth = new FirebaseSimpleLogin(firebase, function (err, user) {
+//      console.log('err, user', err, user);
+//    });
+//    auth.sendPasswordResetEmail(email, function (err, success) {
+//      if (err) {
+//        deferred.reject(err);
+//      } else {
+//        deferred.resolve(success);
+//      }
+//    });
+//
+//    return deferred.promise;
+//
+//  },
+//
+//
+//  return {
+//
+//
+//
+//    logIn: function (user) {
+//      var deferred = $q.defer();
+//
+//      user.rememberMe = true; // Override default session length (browser session) to be 30 days.
+//      firebaseSimpleLogin.$login('password', user).then(deferred.resolve, deferred.reject);
+//
+//      return deferred.promise;
+//    },
+//
+//    logOut: function () {
+//      var deferred = $q.defer();
+//
+//      deferred.resolve(firebaseSimpleLogin.$logout());
+//
+//      return deferred.promise;
+//    },
+//
+//    change: firebaseSimpleLogin.$changePassword
+//
+//  }
+//
+//}
+
+//angular.module('app').service('AuthService', AuthService);
+
+//function userService($q, $firebase, $firebaseSimpleLogin, environmentService, Restangular) {
+//
+//
+//  var env = environmentService.get(),
+//      firebase = new Firebase(env.firebase),
+//      firebaseSimpleLogin = $firebaseSimpleLogin(firebase),
+//      usersRef = $firebase(new Firebase(env.firebase + '/users')),
+//      getCurrentUser = function () {
+//        var deferred = $q.defer();
+//
+//        firebaseSimpleLogin.$getCurrentUser().then(deferred.resolve, deferred.reject);
+//
+//        deferred.promise.then(function (currentUser) {
+//          if (currentUser) {
+//            Restangular.setDefaultHeaders({"Authorization": currentUser.firebaseAuthToken});
+//          }
+//        });
+//
+//        return deferred.promise;
+//      };
+//
+//};
+
+
 angular.module('app').factory('RegistrationService', function ($http, CFG) {
   return {
     signup: function (params, success, error) {
