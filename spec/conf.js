eb3076e6afd12dc6e1a2a67192a5dc002d72afb8
David Conner <dconner.pro@gmail.com>
2014-10-17 04:23:04 -0400

    * removed all the crap that didn't work...

--- spec/conf.js
+++ spec/conf.js
@@ -10,32 +10,12 @@ exports.config = {
     includeStackTrace: true
   },
 
+  //onPrepare: function() { },
+
   params: {
     login: {
       email: 'user123@example.com',
       password: 'password'
     }
-  },
-
-  //mocks: {
-  //  default: [
-  //    'register.create'//,
-  //    //'login.create'
-  //  ]
-  //},
-
-  onPrepare: function() {
-    //var HttpBackend = require('http-backend-proxy');
-    //browser.proxy = new HttpBackend(browser, {buffer:true});
-    browser.get('http://localhost:8000/');
-    //browser.executeScript('angular.module(\'app\').requires.push(\'e2e-mocks\')');
-    //browser.executeScript('angular.module(\'app\', [\'ngMockE2E\']);');
-
-    ////load mocks
-    //browser.mock = require('protractor-http-mock');
-    //browser.mock.config = {
-    //  rootDirectory: __dirname, // default value: process.cwd()
-    //  protractorConfig: 'conf.js' // default value: 'protractor.config'
-    //};
   }
 };
--- spec/users_can_sign_up_spec.js
+++ spec/users_can_sign_up_spec.js
@@ -3,29 +3,21 @@ describe('users can sign up and login', function() {
   var login = browser.params.login;
 
   beforeEach(function() {
+    browser.get('http://localhost:8000/');
   });
 
   it('should offer registration and login via modals on the main page', function() {
+    element(by.css('nav.navbar'))
+        .element(by.buttonText('Sign Up'))
+        .click();
 
-    //console.log(browser.executeScript('console.log("fdsa");'));
-    //console.log(browser.executeScript('console.log(angular);'))
-    //browser.proxy.whenPOST('/auth/sign_in').respond(200);
-    //browser.proxy.flush();
-
-    //element(by.css('nav.navbar'))
-    //    .element(by.buttonText('Sign Up'))
-    //    .click();
-    //
-    //var signup = element(by.css('#signup-modal'));
-    //signup.element(by.model('signup.email')).sendKeys(login.email);
-    //signup.element(by.model('signup.username')).sendKeys(login.username);
-    //signup.element(by.model('signup.password')).sendKeys(login.password);
-    //signup.element(by.model('signup.passwordConfirmation')).sendKeys(login.password);
-    ////signup.element(by.buttonText('Sign Up')).click();
-    //signup.element(by.buttonText('Cancel')).click();
-
-    //console.log(browser.proxy.whenPOST('/auth/sign_up'));
-    //.respond(function(method, url) { return [200, {username:'foobarbaz'}]});
+    var signup = element(by.css('#signup-modal'));
+    signup.element(by.model('signup.email')).sendKeys(login.email);
+    signup.element(by.model('signup.username')).sendKeys(login.username);
+    signup.element(by.model('signup.password')).sendKeys(login.password);
+    signup.element(by.model('signup.passwordConfirmation')).sendKeys(login.password);
+    //signup.element(by.buttonText('Sign Up')).click();
+    signup.element(by.buttonText('Cancel')).click();
 
     element(by.css('nav.navbar'))
         .element(by.buttonText('Log In'))
@@ -35,12 +27,6 @@ describe('users can sign up and login', function() {
     modal.element(by.model('creds.email')).sendKeys(login.email);
     modal.element(by.model('creds.password')).sendKeys(login.password);
     modal.element(by.buttonText('Log In')).click();
-
-    //element(by.model('first')).sendKeys(1);
-    //element(by.model('second')).sendKeys(2);
-    //element(by.id('gobutton')).click();
-    //expect(element(by.binding('latest')).getText()).
-    //    toEqual('5'); // This is wrong!
   });
 
   it('should allow users to bypass the signup process by using google oauth', function() {
