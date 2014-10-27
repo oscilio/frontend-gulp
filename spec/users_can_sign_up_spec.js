809f97bec1c1cc6a1404337867138a9e68f66795
David Conner <dconner.pro@gmail.com>
2014-10-27 15:11:35 -0400

    * added a signout button to the user-profile-widget
    * updated the "Users Can Sign Up Spec" to assert session stickiness and signout.

--- spec/users_can_sign_up_spec.js
+++ spec/users_can_sign_up_spec.js
@@ -7,10 +7,9 @@ describe('users can sign up and login', function() {
   });
 
   it('should offer signup, signin and signout on the main page', function() {
-    var navbar = element(by.css('nav.navbar'))
+    var navbar = element(by.css('nav.navbar'));
 
-    navbar.element(by.buttonText('Sign Up'))
-        .click();
+    navbar.element(by.buttonText('Sign Up')).click();
 
     var signup = element(by.css('#signup-modal'));
     signup.element(by.model('signup.email')).sendKeys(login.email);
@@ -18,20 +17,24 @@ describe('users can sign up and login', function() {
     signup.element(by.model('signup.password')).sendKeys(login.password);
     signup.element(by.model('signup.passwordConfirmation')).sendKeys(login.password);
     signup.element(by.buttonText('Sign Up')).click();
-    //TODO: assertion of $rootScope response to registration event
 
-    navbar.element(by.buttonText('Log In'))
-        .click();
+    navbar.element(by.buttonText('Log In')).click();
 
     var modal = element(by.css('#login-modal'));
     modal.element(by.model('creds.email')).sendKeys(login.email);
     modal.element(by.model('creds.password')).sendKeys(login.password);
     modal.element(by.buttonText('Log In')).click();
 
-    //TODO: assertion of $rootScope response to signin event
-    expect(navbar.element(by.css('#user-profile-widget'))).toContain(login.username);
+    expect(navbar.element(by.css('#user-profile-widget')).getText()).toMatch(login.email);
 
-    //TODO: signout assertions
+    // refresh & assert auth is persisted
+    browser.get('http://localhost:8000/');
+    var userProfileWidget = element(by.css('#user-profile-widget')),
+        signoutButton = userProfileWidget.element(by.css('#user-profile-logout'));
+    expect(userProfileWidget.getText()).toMatch(login.email);
+
+    signoutButton.click();
+    expect(element(by.css('nav.navbar')).getText()).toMatch("Sign Up");
   });
 
   it('should allow users to bypass the signup process by using google oauth', function() {
